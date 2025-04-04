186b181ed0bb546bcd61d8df34b406cdb2b46bd8
Pinhan Zhao <jas4ever@bu.edu>
2025-04-04 15:41:50 -0400

    Update the button style.

--- components/header.tsx
+++ components/header.tsx
@@ -1,27 +1,18 @@
 "use client";
 import Link from "next/link";
-import styled from "styled-components";
 
-const StyledHeader = styled.h2`
-    font-size: 3rem;
-`;
-
-const StyledLink = styled(Link)`
-    font-size: 1rem;
-    padding: 0.5rem;
-`;
 
 export default function Header() {
     return (
         <header style={{margin: '2%'}}>
-            <StyledHeader>CS391 WEATHER API</StyledHeader>
+            <h1 style={{fontSize: '3rem'}}>CS391 WEATHER API</h1>
             <nav >
-                <StyledLink href="/">
+                <Link style={{color: 'red', padding: '2%', fontSize: '1.5rem', textDecoration: 'none'}} href="/">
                     Home
-                </StyledLink>
-                <StyledLink href="/about">
+                </Link>
+                <Link style={{color: 'red', padding: '2%', fontSize: '1.5rem', textDecoration: 'none'}} href="/about">
                     About
-                </StyledLink>
+                </Link>
             </nav>
         </header>
     );
